2e4ec4e2b1684cefabd38be644b47d1f90ece93b
yannickwesley <yannickkarimou@gmail.com>
2021-02-05 11:56:10 +0000

    tp_03 reussi

---
+++ src/java/utilisateurs/gestionnaires/GestionnaireUtilisateurs.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilisateurs.gestionnaires;
+
+import java.util.Collection;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import utilisateurs.modeles.Utilisateur;
+
+/**
+ *
+ * @author kykwy
+ */
+@Stateless
+public class GestionnaireUtilisateurs {
+
+       @PersistenceContext
+    private EntityManager em;
+
+    public void creerUtilisateursDeTest() {
+        creeUtilisateur("John", "Lennon", "jlennon");
+        creeUtilisateur("Paul", "Mac Cartney", "pmc");
+        creeUtilisateur("Ringo", "Starr", "rstarr");
+        creeUtilisateur("Georges", "Harisson", "georgesH");
+    }
+
+    public Utilisateur creeUtilisateur(String nom, String prenom, String login) {
+        Utilisateur u = new Utilisateur(nom, prenom, login);
+        em.persist(u);
+        return u;
+    }
+
+    public Collection<Utilisateur> getAllUsers() {
+        // Exécution d'une requête équivalente à un select *
+        Query q = em.createQuery("select u from Utilisateur u");
+        return q.getResultList();
+    }
+}
